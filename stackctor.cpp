137d4cb5eebd555ece13e7e2dc6287772a9e1aaa
sasha_oiya_ssanina <141181890+SashaOiya@users.noreply.github.com>
2023-09-28 22:48:43 +0300

    Add files via upload

--- stackctor.cpp
+++ stackctor.cpp
@@ -27,40 +27,63 @@ enum Errors_t {
 int StackCtor ( char **str, int size_stack, int capacity );
 void StackDump ( const char *begine_line, const char* func_name, const char* file_name,
                  size_t size_stack, size_t capacity );
-int StackDetore ( char *begine_line, size_t size_stack );  // Dtor Destructor DestructStack ConstructStack
-Errors_t Pop ( char **str_1, char **str_2 );   // StackPop
+int StackDetore ( char **begine_line, size_t size_stack );  // Dtor Destructor DestructStack ConstructStack
+Errors_t Pop ( char *str_1, char *str_2 );   // StackPop
 Errors_t Push ( char **str, char **str_2, const char value, int * size_stack, int capacity ); // StackPush
 
 int main()
 {
-    char *str;             //free
-    char *begine_line = str;
-    char *str_2; // stack_data {}
-    char c = 0;
-
     int capacity = 0;
     int size_stack = 2;  // delete 1
 $                            // push
+    char *str = (char *)calloc ( size_stack, sizeof ( char) );            //free
+    char *begine_line = str;
+    char *str_2 = (char *)calloc ( size_stack, sizeof ( char) ); // stack_data {}
+    char c = 0;
+
     for ( int i = 0; ( c = getchar() ) != '\n'; ++ i ) {
         ++capacity;
-        printf ("size %d capacity %d value %c\n", size_stack, capacity, c );
-$       str = str + capacity;  //
-$       Push ( &str, &str_2, c, &size_stack, capacity );
-$       //Pop ( &str, &str_2 );
+        if ( capacity == size_stack ) {
+            size_stack = 2 * (size_stack);
+
+$           char *ptr = (char *)calloc( size_stack + 1, sizeof( char ) );
+$           //memset ( ptr, 3, size_stack );
+            StackDetore ( &ptr, size_stack );
+
+            printf ("address %p\t%s size %d\n", ptr, ptr, strlen(ptr) );
+
+            strcpy ( ptr, str );
+
+$           str = ptr;
+            //free ( ptr );
+
+            char *ptr_2 = (char *)calloc( size_stack + 1, sizeof( char ) );
+$           StackDetore ( &ptr_2, size_stack );
+            //memset ( ptr_2, 3, size_stack );
+            //Pop ( &str, &str_2 );
+
+            printf ("address %p\t%s size %d\n", ptr_2, ptr_2, strlen(ptr) );
+
+            strcpy ( ptr_2, str_2 );
+            str_2 = ptr_2;
+            //free ( ptr_2 );
+$       }
+        //Push ( &str, &str_2, c, &size_stack, capacity );
+        str[i] = c;
+        printf ("size %d capacity %d value %c str %s \n", size_stack, capacity, c, str );
+        Pop ( str + i, str_2 + i );
     }
 
-    /*Stack_Data_t Stack;
-    Stack.str = strdup ( "Meow" );
-    Stack.begine_line = Stack.str;   */
 $
-    printf ( "<%p>\t%s\t%c\n", str, str, *str );
-$   begine_line = str - capacity;
 
     //int error_name = StackCtor( (void **)&begine_line, size_stack, capacity );   //
 
     //pop
 
-    StackDump ( begine_line, __PRETTY_FUNCTION__, __FILE__, size_stack, capacity );
+    StackDump ( str_2, __PRETTY_FUNCTION__, __FILE__, size_stack, capacity );
+
+    free ( str );
+    free ( str_2 );
 
     return 0;
 }
@@ -108,57 +131,42 @@ $   printf ( "Stack [%p] "" 'stk' called from%s\n "   // from main.cpp
              " \t\tsize = %d \n "
              " \t\tcapacity = %d \n "
              " \t\tdata [%p]:\n", begine_line , file_name, func_name, size_stack, capacity, begine_line  ); // data
-$   for ( int i = 0; i < capacity; ++i ) { // < or <=
+$   for ( int i = 0; i < size_stack; ++i ) { // < or <=
         printf ( "\t\tdata[%d] = %c \n", i, begine_line[i] );
     }
     printf ( "\t}\n}\n");
 }
 
-int StackDetore ( char *begine_line, size_t size_stack )      // plus errors check
+int StackDetore ( char **begine_line, size_t size_stack )      // plus errors check
 {
-    assert ( begine_line != nullptr );
+    assert ( *begine_line != nullptr );
 
-//    free ( begine_line );
-
-$   memset ( begine_line, 3, size_stack * sizeof ( char ) );
+$   memset ( *begine_line, 3, size_stack * sizeof ( char ) );
 
     return NO_ERRORS;
 }
 
-Errors_t Push ( char **str, char **str_2, const char value, int * size_stack, int capacity )   // pointer
+Errors_t Push ( char *str[], char **str_2, const char value, int * size_stack, int capacity )   // pointer
 {
-$   char *start = *str;
-    *str = *str - capacity + 1;
-    printf ("%p\n", *str);
-    if ( capacity == *size_stack ) {             // sizeof ( str )
-        printf ("PUSH : size %d capacity %d\n", *size_stack, capacity );
-        printf ("str, value : %s\t%c\n", *str, value );
-$       //*size_stack = StackCtor ( str, *size_stack, capacity );   //
-        *size_stack = 2 * *size_stack;
-$        char *ptr = (char *)calloc( *size_stack, sizeof( char ) );
-$        memset ( ptr, 15, sizeof ( char ) );
+
+    if ( capacity == *size_stack ) {
+        *size_stack = 2 * (*size_stack);
+$       char *ptr = (char *)calloc( *size_stack + 1, sizeof( char ) );
+$       memset ( ptr, 3, *size_stack );
+        printf ("address %p\t%s size %d str %s\n", ptr, ptr, strlen(ptr), *str );
         strcpy ( ptr, *str );
-
-        /*char *ptr_2 = (char *)calloc( *size_stack, sizeof( char ) );
-        strcpy ( ptr_2, *str_2 );   */
-
-        printf ("%p\n", ptr);
-$       // strcpy ( ptr, *str );
-$        *str = ptr;
+        *str = ptr;
 $    }
 
-$    *str = start;
-$    **str = value;
-$    ++*str;
-    //printf ("str : %p\n", *str );
+$   *str[capacity-1] = value;
 
     return NO_ERRORS;   //errors
 }
 
-Errors_t Pop ( char **str_1, char **str_2 )   // pointer     const
+Errors_t Pop ( char *str_1, char *str_2 )   // pointer     const
 {
     //--str_1;
-    **str_2 = **str_1;     //assert
+    *str_2 = *str_1;     //assert
 
     return NO_ERRORS;   //errors
 }
